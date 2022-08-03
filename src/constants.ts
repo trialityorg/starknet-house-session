5d8b94fa739195a604b1391d3efe197ccef17b6d
Matt Coleman <m-coleman@users.noreply.github.com>
2022-08-03 07:28:19 -0700

    Comments

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,7 @@
+// address of the contract
 export const SUM_GAME_CONTRACT_ADDRESS = '0x0696dac08a34f8e5ed7d35a5053a6e72e18eae5b6314e48f26d5c00801d83a6f';
 
+// functions in the contract
 export const VERIFY_SUM_ENTRYPOINT = "verifySum";
 export const GET_CURR_NUMBER_ENTRYPOINT = "getCurrNumber";
 export const GET_IS_ACTIVE_ENTRYPOINT = "getIsActive";
